98bfe58caff0b5b165fdb4bdd3b4c41417020eda
RehanAli7024 <bb1221388@iitd.ac.in>
2024-03-25 02:49:29 +0530

    Login Page fucntionality

--- src/components/account/login.jsx
+++ src/components/account/login.jsx
@@ -3,11 +3,21 @@ import "./login.css";
 import { useState } from "react";
 const Login = () => {
   const [loginSignup, setloginSignup] = useState("login");
+  const [signupvalues, setsignupvalues] = useState({
+    name: "",
+    username: "",
+    password: "",
+  });
   const imageURL =
     "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
-    const toggleloginsignup = ()=>{
-        loginSignup === "login" ? setloginSignup("signup") : setloginSignup("login")
-    }
+  const toggleloginsignup = () => {
+    loginSignup === "login"
+      ? setloginSignup("signup")
+      : setloginSignup("login");
+  };
+  const oninputchange = (e) => {
+    setsignupvalues({ ...signupvalues, [e.target.name]: e.target.value });
+  };
   return (
     <Box className="loginBox">
       <Box>
@@ -22,20 +32,43 @@ const Login = () => {
               Login
             </Button>
             <Typography className="loginOR">OR</Typography>
-            <Button variant="text" id="loginCreateAcc" onClick={()=> toggleloginsignup()}>
+            <Button
+              variant="text"
+              id="loginCreateAcc"
+              onClick={() => toggleloginsignup()}
+            >
               Create an account
             </Button>
           </Box>
         ) : (
           <Box className="loginFieldsContainor">
-            <TextField variant="standard" label="Enter Name" />
-            <TextField variant="standard" label="Enter Username" />
-            <TextField variant="standard" label="Enter Password" />
+            <TextField
+              variant="standard"
+              onChange={(e) => oninputchange(e)}
+              name="name"
+              label="Enter Name"
+            />
+            <TextField
+              variant="standard"
+              onChange={(e) => oninputchange(e)}
+              name="username"
+              label="Enter Username"
+            />
+            <TextField
+              variant="standard"
+              onChange={(e) => oninputchange(e)}
+              name="password"
+              label="Enter Password"
+            />
             <Button variant="contained" id="loginButton">
               SignUp
             </Button>
             <Typography className="loginOR">OR</Typography>
-            <Button variant="text" id="loginCreateAcc" onClick={()=>toggleloginsignup()}>
+            <Button
+              variant="text"
+              id="loginCreateAcc"
+              onClick={() => toggleloginsignup()}
+            >
               Already have an account
             </Button>
           </Box>
